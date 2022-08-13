b846516229f50e192ce38503fea911429ecd6a51
Andres Osorio <af.osorio1341@gmail.com>
2022-08-13 16:08:50 +0200

    Fix allowed headers

--- internal/transport/http/middleware.go
+++ internal/transport/http/middleware.go
@@ -60,7 +60,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-Auth-Token, Authorization")
+			w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Api-Key")
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 			return
